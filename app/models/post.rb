de99076804e2f0b075b64b4f9eca31666b1ae76a
Felipe Guerra <sixto.guerra1982@gmail.com>
2022-06-28 20:33:36 -0400

    spoiler method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
     validates :image_url , presence: :true
     validates :title , presence: :true
     validates :content , presence: :true
+
+    before_save :update_content
+
+    def update_content
+        # censurar la palabra spoiler
+        acum = ""
+        array = self.content.split
+        array.each do |palabra|
+            acum += "#{palabra} " if palabra.upcase != "SPOILER"
+        end
+        self.content = acum
+    end
+
 end
